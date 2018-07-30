fcd46223caa6ff5a72084b5be8758c8be1d49d80
Ciaran Morinan <cmorinan@gmail.com>
2018-07-30 18:31:26 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,14 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   pigeons = {}
-  data.each do 
+  data.each do |attribute, details|
+    details.each do |detail, name_array|
+      name_array.each do |name|
+        pigeons[name] ||= {}
+        pigeons[name][attribute] ||= []
+        pigeons[name][attribute] << detail
+      end
+    end
+  end
 end
